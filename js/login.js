cee97e3e6055d449bc7e0e76731d5a0a2f82ed14
habibapanna <habibapanna49@gmail.com>
2024-09-20 00:19:25 +0600

    added a home.html file

--- js/login.js
+++ js/login.js
@@ -4,12 +4,39 @@
 
 // search: form submit reloading the page
 // step-1: set event handler
-document.getElementById('button-login')
-.addEventListener('click', function(event){
-    // step-2: prevent default behavior (prevent reloading browser)
+// document.getElementById('button-login')
+// .addEventListener('click', function(event){
+//     // step-2: prevent default behavior (prevent reloading browser)
+//     event.preventDefault();
+//     console.log('login button clicked');
+//     // step-3: get the phone number
+//     const phoneNumber = document.getElementById('phone-number').value;
+//     const pinNumber = document.getElementById('pin-number').value;
+//     console.log(phoneNumber, pinNumber);
+// // step-4: validate phone and pin
+//     if(phoneNumber === '5' && pinNumber ==='1234'){
+//         console.log('you are logged in');
+//         // step-5: allow user to use the website
+//     }
+//     else{
+//        alert('wrong phone number or pin') 
+//     }
+// })
+
+document.getElementById('button-login').addEventListener('click',function(event){
+    // console.log('button-clicked')
     event.preventDefault();
-    console.log('login button clicked');
-    // step-3: get the phone number
+
+    // get phone number and pin
     const phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber);
-})
+    const pinNumber = document.getElementById('pin-number').value;
+    // console.log(phoneNumber, pinNumber);
+
+    if(phoneNumber === '5' && pinNumber === '1234'){
+        console.log('you are logged in')
+        window.location.href = '/home.html';
+    }
+    else{
+        alert('wrong phone number or pin')
+    }
+});
